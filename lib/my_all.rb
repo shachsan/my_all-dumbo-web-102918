7e4e62af81c34184f0d049d6fe34b3e229f537c6
SANJAY K SAI <shachsan@hotmail.com>
2018-10-30 13:16:43 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_returned=[]
+  collection.each do |ele|
+    block_returned << yield(ele)
+  end
+  block_returned.all?{|e| e ? true : false}
 end
+
+# p my_all?([1,2,3]) {|i| i < 2}
