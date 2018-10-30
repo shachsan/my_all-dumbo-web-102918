fbe64db13213314568c33ea91dec3697c6c62d74
SANJAY K SAI <shachsan@hotmail.com>
2018-10-30 13:16:32 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,7 @@ def my_all?(collection)
   collection.each do |ele|
     block_returned << yield(ele)
   end
-  block_returned.all?{|e|if e ? true : false}
+  block_returned.all?{|e| e ? true : false}
 end
 
 # p my_all?([1,2,3]) {|i| i < 2}
